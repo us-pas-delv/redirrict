9d0a5fee8da62c72f56ace679147d2fff3de2a37
us-pas-delv <168684031+us-pas-delv@users.noreply.github.com>
2026-04-10 11:04:29 +0100

    index_sa.php

---
+++ index_sa.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Redirecting...</title>
+</head>
+<body>
+    <p>Redirecting in 3 seconds...</p>
+
+    <script>
+        setTimeout(function() {
+            window.location.href = "https://www.po-rum-bo.com/buy-it-now/done/SPL_2k26/sa/";
+        }, 3000);
+    </script>
+</body>
+</html>
